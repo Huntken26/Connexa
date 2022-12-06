f2488d006ff76a655fe3af668816a0dca8637aed
lisaericksoncoding <111710705+lisaericksoncoding@users.noreply.github.com>
2022-12-05 22:05:17 -0600

    proper folder structures

---
+++ db/schema.sql
@@ -0,0 +1,22 @@
+-- DROP DATABASE
+DROP DATABASE IF EXISTS user_db;
+
+-- CREATE DATABASE
+CREATE DATABASE user_db;
+
+CREATE TABLE `blog` . `user` (
+    `id` BIGINT NOT NULL AUTO_INCREMENT,
+    `firstName` VARCHAR(50) NULL DEFAULT NULL,
+    `lastName` VARCHAR(50) NULL DEFAULT NULL,
+    `email` VARCHAR(50) NULL DEFAULT NULL,
+    `userName` VARCHAR(50) NULL DEFAULT NULL,
+    `passwordHash` VARCHAR(32) NOT NULL,
+    `registeredAt` DATETIME NOT NULL,
+    `lastLogin` DATETIME NULL DEFAULT NULL,
+    `intro` TINYTEXT NULL DEFAULT NULL,
+    `profile` TEXT NULL DEFAULT NULL,
+    `strengths` TEXT NULL DEFAULT NULL,
+    PRIMARY KEY (`id`),
+    
+
+)
